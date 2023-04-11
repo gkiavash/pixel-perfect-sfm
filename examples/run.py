5f000622ea5026123f8cafdfc4e06c1132657c47
gkiavash <gkiavash@gmail.com>
2023-04-11 12:05:21 +0200

    3

--- examples/run.py
+++ examples/run.py
@@ -13,6 +13,7 @@ BASE_DIR = Path(BASE_DIR)
 path_images = BASE_DIR / 'images'
 path_keypoints = BASE_DIR / 'keypoints.h5'
 path_pairs = BASE_DIR / 'pairs.txt'
+path_matches = BASE_DIR / 'matches.h5'
 
 keypoints_conf = extract_features.confs["r2d2"]
 matcher_conf = match_features.confs["superglue"]
@@ -26,4 +27,11 @@ extract_features.main(
 pairs_from_exhaustive.main(
     output=path_pairs,
     features=path_keypoints,
+)
+
+match_features.main(
+    conf=matcher_conf,
+    pairs=path_pairs,
+    features=path_keypoints,
+    matches=path_matches
 )
