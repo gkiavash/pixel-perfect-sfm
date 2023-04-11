3eab284eb40f924cfeea1c55a9fdb3ed27c3f299
gkiavash <gkiavash@gmail.com>
2023-04-11 12:36:44 +0200

    5

--- examples/run.py
+++ examples/run.py
@@ -5,6 +5,7 @@ from hloc import (
     match_features,
     pairs_from_exhaustive
 )
+from pixsfm.refine_hloc import PixSfM
 
 
 BASE_DIR = sys.argv[1]
@@ -14,6 +15,8 @@ path_images = BASE_DIR / 'images'
 path_keypoints = BASE_DIR / 'keypoints.h5'
 path_pairs = BASE_DIR / 'pairs.txt'
 path_matches = BASE_DIR / 'matches.h5'
+path_sfm = BASE_DIR / 'sfm/'
+path_cache = BASE_DIR / f'dense_features_cache.h5'
 
 keypoints_conf = extract_features.confs["r2d2"]
 matcher_conf = match_features.confs["superglue-fast"]
@@ -35,4 +38,14 @@ match_features.main(
     pairs=path_pairs,
     features=path_keypoints,
     matches=path_matches
+)
+
+refiner = PixSfM()
+reconstruction, sfm_outputs = refiner.triangulation(
+    output_dir=path_sfm,
+    image_dir=path_images,
+    pairs_path=path_pairs,
+    features_path=path_keypoints,
+    matches_path=path_matches,
+    cache_path=path_cache,
 )
