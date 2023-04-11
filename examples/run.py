f6d0e1a5e5f061f750a3234f802760d108ca3fd7
gkiavash <gkiavash@gmail.com>
2023-04-11 11:57:19 +0200

    2

--- examples/run.py
+++ examples/run.py
@@ -1,6 +1,10 @@
 import sys
 from pathlib import Path
-from hloc import extract_features, match_features
+from hloc import (
+    extract_features,
+    match_features,
+    pairs_from_exhaustive
+)
 
 
 BASE_DIR = sys.argv[1]
@@ -8,9 +12,18 @@ BASE_DIR = Path(BASE_DIR)
 
 path_images = BASE_DIR / 'images'
 path_keypoints = BASE_DIR / 'keypoints.h5'
+path_pairs = BASE_DIR / 'pairs.txt'
 
 keypoints_conf = extract_features.confs["r2d2"]
 matcher_conf = match_features.confs["superglue"]
 
-# extract sparse local features for mapping and query images
-extract_features.main(keypoints_conf, path_images, feature_path=path_keypoints)
+extract_features.main(
+    keypoints_conf,
+    path_images,
+    feature_path=path_keypoints
+)
+
+pairs_from_exhaustive.main(
+    output=path_pairs,
+    features=path_keypoints,
+)
